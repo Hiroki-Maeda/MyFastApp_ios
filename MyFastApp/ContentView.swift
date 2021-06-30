96c410f54fca942e683cc8bad8138eafd7efb9d6
Hiroki-Maeda <q238059h@mail.kyutech.jp>
2021-06-30 17:49:56 +0900

    Initial Commit

---
+++ MyFastApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFastApp
+//
+//  Created by 前田　浩貴 on 2021/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFastApp/MyFastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFastAppApp.swift
+//  MyFastApp
+//
+//  Created by 前田　浩貴 on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct MyFastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
